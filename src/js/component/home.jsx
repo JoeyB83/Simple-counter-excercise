92c7505da63137551ebb7a789d31d7f7794c4dad
Joel Barranco <joealebarranco@gmail.com>
2022-09-30 14:50:01 -0300

    Simple Counter

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./index.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
+function Home (props) {
+	const [counter, setCounter] = useState(0);
+	useEffect(() => {
+		const interval = setInterval(() => {
+		  setCounter((prevCounter) => prevCounter + 1);
+		}, 1000);
+	
+		return () => clearInterval(interval);
+	  }, []);
+
+	  const two = Math.floor(counter/10);
+	  const three = Math.floor(counter/100);
+	  const four = Math.floor(counter/1000);
+	  const five = Math.floor(counter/10000);
+	  const six = Math.floor(counter/100000);
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className = "counter">
+			<div className = "clock"><FontAwesomeIcon icon = { faClock } /></div>
+			<div className = "digitSix">{six % 10}</div>
+			<div className = "digitFive">{five % 10}</div>
+			<div className = "digitFour">{four % 10}</div>
+			<div className = "digitThree">{three % 10}</div>
+			<div className = "digitTwo"> {two % 10}</div>
+			<div className = "digitOne"> {counter % 10}</div>
+		</div>			
 	);
-};
+};	
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,10 @@
 //import react into the bundle
+import { counter } from "@fortawesome/fontawesome-svg-core";
 import React from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
-import "../styles/index.css";
+
 
 //import your own components
 import Home from "./component/home.jsx";
